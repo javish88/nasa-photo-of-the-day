3c41a21f7d14e1128121889f9fa6a905f9ea7ecf
Javier <tsakasu88@gmail.com>
2019-11-06 16:01:34 -0500

    working on fixing axios issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
+import PhotoCard from "../Components/PhotoCard";
 
 function App() {
+
+  const [nasaImg, setNasaImg] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DRciVBnKsadw32Qr1nN7EpkTtestJuBd3odCGEuMy")
+    .then(response => {
+      console.log(response.data);
+      setNasaImg(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      
+        {/* Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀! */}
+        <header><h1>NASA PHOTO OF THE DAY BABY!!!!!!!!!</h1></header>
+        <PhotoCard title={nasaImg.title} url={nasaImg.url} explanation={nasaImg.explanation} date={nasaImg.date}/>
+      
     </div>
   );
 }
