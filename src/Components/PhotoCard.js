faa7291df83a68a9c8ed44968a042674e6216f09
Javier <tsakasu88@gmail.com>
2019-11-07 19:07:28 -0500

    added styling components

--- src/Components/PhotoCard.js
+++ src/Components/PhotoCard.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const PhotoCardSpace = styled.div`
+background: #99f3eb;
+color: black;
+border-radius: 50%;
+margin: 10px;
+transition: transform 0.2s ease-in;
+width: 100%;
+display: flex;
+flex-direction: column;
+
+&:hover {
+  transform: translate(-5px) scale(1.065);
+}
+`;
+
+
+
 
 const PhotoCard = (props) => {
     return (
-        <div>
+        <PhotoCardSpace>
             <h1>
                 {props.title}
             </h1>
@@ -10,15 +29,15 @@ const PhotoCard = (props) => {
                 {props.date}
             </h2>
             <img src = {props.url} alt ="Nasa Pictures"/>
-            <div>
+           
                 <h3>
                     Details
                 </h3>
                 <p>
                     {props.explanation}
                 </p>
-            </div>
-        </div>
+           
+                </PhotoCardSpace>
     );
 };
 
